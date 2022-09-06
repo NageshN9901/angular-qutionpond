bd0922ac66de83d6d85da03c72b62666833699ae
CORP\Nagesh.N <nagesh.n@tangoe.com>
2022-09-07 08:26:51 +1245

    ji

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'HospitalMangementSystem';
+
+  // here  we are doing binding
+  someValue:string="";
+  someValues:Array<string>=new Array<string>();// or we can use java script coe like somVlues=[];/ to collcet all the values in the name of somevalue
+  //
+  CallSomeLogic(){
+    //alert("hello")
+    // adding below somecvalues from somevalue
+    this.someValues.push(this.someValue);// here this what ever enetr values are somevalues to push into somevalues colection
+    // we need clear text box also after clciking or stroing the data
+    this.someValue="";// here after click buttion done means no data in text box to clear the text box
+
+  }
 }
